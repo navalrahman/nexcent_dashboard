bf7d1e169f069f8203cfaaed59ad24116b9b3959
navalrahman <77826561+navalrahman@users.noreply.github.com>
2025-06-09 15:50:43 +0530

    Update Thumbnailsix.jsx

--- src/components/dashboard/thumbnailsix/Thumbnailsix.jsx
+++ src/components/dashboard/thumbnailsix/Thumbnailsix.jsx
@@ -18,6 +18,7 @@ const Thumbnailsix = () => {
 
     const fetchData = async () => {
         try {
+            // const response = await axios.get('http://localhost:9000/api/six/alldata',
             const response = await axios.get('http://localhost:9000/api/six/alldata',
                 { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
             )
@@ -50,7 +51,8 @@ const Thumbnailsix = () => {
                 url
             }
 
-            const response = await axios.put(`http://localhost:9000/api/five/update/${id}`,
+            // const response = await axios.put(`http://localhost:9000/api/five/update/${id}`,
+            const response = await axios.put(`https://nexcent-backend-4vbo.onrender.com/api/five/update/${id}`,
                 updatedData,
                 {
                     headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
