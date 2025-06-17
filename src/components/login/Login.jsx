97eadad09f6c908618d8a1b5b72203bc31c0cc0b
navalrahmanlevon <navalrahman.levontechno@gmail.com>
2025-06-17 09:47:21 +0530

    new changes

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -17,13 +17,13 @@ const Login = () => {
     e.preventDefault()
     console.log(data);
     try {
-      const response = await axios.post('http://localhost:9000/api/user/login', data)
+      // const response = await axios.post('http://localhost:9000/api/user/login', data)
       // const response = await axios.post('https://nexcent-backend-4vbo.onrender.com/api/user/login', data)
       // const response = await axios.post('http://localhost:9000/api/user/login', data)
       //  const response = await axios.post('https://nexcent-backend-4vbo.onrender.com/api/user/login', data)
 {/*       const response = await axios.post('http://localhost:9000/api/user/login', data) */}
       
-      //  const response = await axios.post('https://nexcent-backend-4vbo.onrender.com/api/user/login', data)
+       const response = await axios.post('https://nexcent-backend-4vbo.onrender.com/api/user/login', data)
       console.log(response);
       alert(response.data.status)
       setTimeout(() => {
